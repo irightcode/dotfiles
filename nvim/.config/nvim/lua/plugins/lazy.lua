90aa6a70b1505b0a6231194ec36af6ba9808a894
irightcode <irightcode@github.com>
2024-03-07 18:21:10 -0500

    Remove used neovim plugins

--- nvim/.config/nvim/lua/plugins/lazy.lua
+++ nvim/.config/nvim/lua/plugins/lazy.lua
@@ -16,7 +16,6 @@ vim.opt.rtp:prepend(lazypath)
 vim.o.termguicolors = true
 
 require('lazy').setup({
-  'jackMort/ChatGPT.nvim',
   {
     'Exafunction/codeium.vim',
     config = function ()
@@ -33,11 +32,6 @@ require('lazy').setup({
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
-  },
-  "preservim/vim-pencil",
-  {
-    "sourcegraph/sg.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
   },
   {
     "epwalsh/obsidian.nvim",
@@ -65,36 +59,32 @@ require('lazy').setup({
   },
   'folke/zen-mode.nvim',
   'David-Kunz/gen.nvim',
-  'tpope/vim-dadbod',
   'tpope/vim-obsession',
-  'kristijanhusak/vim-dadbod-ui',
-  'kristijanhusak/vim-dadbod-completion',
 
   -- Tree
   {
     "nvim-tree/nvim-tree.lua",
     version = "*",
     lazy = false,
-    requires = {
+    dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
     config = function()
       require("nvim-tree").setup {}
     end,
   },
-  -- Database
-  {
-    "tpope/vim-dadbod",
-    opt = true,
-    requires = {
-      "kristijanhusak/vim-dadbod-ui",
-      "kristijanhusak/vim-dadbod-completion",
-    },
-    config = function()
-      require("config.dadbod").setup()
-    end,
-  },
-
+  -- -- Database
+  -- {
+  --   "tpope/vim-dadbod",
+  --   dependencies = {
+  --     "kristijanhusak/vim-dadbod-ui",
+  --     "kristijanhusak/vim-dadbod-completion",
+  --   },
+  --   config = function()
+  --     require("config.dadbod").setup()
+  --   end,
+  -- },
+  --
   'ThePrimeagen/git-worktree.nvim',
   'tpope/vim-surround',
   'xiyaowong/nvim-transparent',
@@ -112,32 +102,6 @@ require('lazy').setup({
         y = 0.5
       }
     })
-    end
-  },
-
-  {
-    'rmagatti/goto-preview',
-    config = function()
-      require('goto-preview').setup {
-        width = 120; -- Width of the floating window
-        height = 15; -- Height of the floating window
-        border = {"↖", "─" ,"┐", "│", "┘", "─", "└", "│"}; -- Border characters of the floating window
-        default_mappings = true;
-        debug = false; -- Print debug information
-        opacity = nil; -- 0-100 opacity level of the floating window where 100 is fully transparent.
-        resizing_mappings = false; -- Binds arrow keys to resizing the floating window.
-        post_open_hook = nil; -- A function taking two arguments, a buffer and a window to be ran as a hook.
-        references = { -- Configure the telescope UI for slowing the references cycling window.
-          telescope = require("telescope.themes").get_dropdown({ hide_preview = false })
-        };
-        -- These two configs can also be passed down to the goto-preview definition and implementation calls for one off "peak" functionality.
-        focus_on_open = true; -- Focus the floating window when opening it.
-        dismiss_on_move = false; -- Dismiss the floating window when moving the cursor.
-        force_close = true, -- passed into vim.api.nvim_win_close's second argument. See :h nvim_win_close
-        bufhidden = "wipe", -- the bufhidden option to set on the floating window. See :h bufhidden
-        stack_floating_preview_windows = true, -- Whether to nest floating windows
-        preview_window_title = { enable = true, position = "left" }, -- Whether 
-      }
     end
   },
 
@@ -202,8 +166,20 @@ require('lazy').setup({
     }
   },
 
-  'ray-x/go.nvim',
-  'ray-x/guihua.lua',
+  {
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  },
   {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
@@ -248,12 +224,10 @@ require('lazy').setup({
   'theHamsta/nvim-dap-virtual-text',
   'leoluz/nvim-dap-go',
 
-
   -- Git related plugins
   'tpope/vim-fugitive',
   'lewis6991/gitsigns.nvim',
 
-  'navarasu/onedark.nvim', -- Theme inspired by Atom
   'nvim-lualine/lualine.nvim', -- Fancier statusline
   { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
   'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines 
@@ -290,7 +264,9 @@ require('lazy').setup({
       { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
     },
   },
-  { "catppuccin/nvim", as = "catppuccin" },
+
+  'navarasu/onedark.nvim', -- Theme inspired by Atom
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "folke/twilight.nvim",
     opts = {
