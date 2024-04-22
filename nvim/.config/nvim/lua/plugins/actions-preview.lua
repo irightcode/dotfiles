c73368ec99f3094e666468c9591cfa0960c7e1e3
irightcode <irightcode@gmail.com>
2024-04-22 09:51:44 -0400

    Disabled action preview to fix gf

--- nvim/.config/nvim/lua/plugins/actions-preview.lua
+++ nvim/.config/nvim/lua/plugins/actions-preview.lua
@@ -1,6 +1,8 @@
 return {
   "aznhe21/actions-preview.nvim",
   lazy = false,
+  disabled = true,
+  enabled = false,
   opts = {},
   config = function()
     vim.keymap.set({ "v", "n" }, "gf", require("actions-preview").code_actions)
