fca02debeb98889dde423c11e7d0fd9d1c22410d
irightcode <irightcode@gmail.com>
2024-12-20 03:05:24 -0500

    Fixed cal.sh

--- bin/.local/bin/cal.sh
+++ bin/.local/bin/cal.sh
@@ -4,24 +4,23 @@ ALERT_IF_IN_NEXT_MINUTES=10
 ALERT_POPUP_BEFORE_SECONDS=10
 NERD_FONT_FREE="󱁕 "
 NERD_FONT_MEETING="󰤙"
-DATE=gdate
 
 get_attendees() {
 	attendees=$(
-		icalBuddy \
-			--includeEventProps "attendees" \
-			--propertyOrder "datetime,title" \
-			--noCalendarNames \
-			--dateFormat "%A" \
-			--includeOnlyEventsFromNowOn \
-			--limitItems 1 \
-			--excludeAllDayEvents \
-			--separateByDate \
-			--excludeEndDates \
-			--bullet "" \
-			--excludeCals "training,omerxx@gmail.com" \
-			eventsToday
-	)
+	icalBuddy \
+		--includeEventProps "attendees" \
+		--propertyOrder "datetime,title" \
+		--noCalendarNames \
+		--dateFormat "%A" \
+		--timeFormat "%H:%M" \
+		--includeOnlyEventsFromNowOn \
+		--limitItems 1 \
+		--excludeAllDayEvents \
+		--separateByDate \
+		--excludeEndDates \
+		--bullet "" \
+		--excludeCals "training,omerxx@gmail.com" \
+		eventsToday)
 }
 
 parse_attendees() {
@@ -29,7 +28,7 @@ parse_attendees() {
 	for line in $attendees; do
 		attendees_array+=("$line")
 	done
-	number_of_attendees=$((${#attendees_array[@]} - 3))
+	number_of_attendees=$((${#attendees_array[@]}-3))
 }
 
 get_next_meeting() {
@@ -38,6 +37,7 @@ get_next_meeting() {
 		--propertyOrder "datetime,title" \
 		--noCalendarNames \
 		--dateFormat "%A" \
+		--timeFormat "%H:%M" \
 		--includeOnlyEventsFromNowOn \
 		--limitItems 1 \
 		--excludeAllDayEvents \
@@ -48,21 +48,21 @@ get_next_meeting() {
 }
 
 get_next_next_meeting() {
-	end_timestamp=$($DATE +"%Y-%m-%d ${end_time}:01 %z")
-	tonight=$($DATE +"%Y-%m-%d 23:59:00 %z")
+	end_timestamp=$(date +"%Y-%m-%d ${end_time}:01 %z")
+	tonight=$(date +"%Y-%m-%d 23:59:00 %z")
 	next_next_meeting=$(
-		icalBuddy \
-			--includeEventProps "title,datetime" \
-			--propertyOrder "datetime,title" \
-			--noCalendarNames \
-			--dateFormat "%A" \
-			--limitItems 1 \
-			--excludeAllDayEvents \
-			--separateByDate \
-			--bullet "" \
-			--excludeCals "training,omerxx@gmail.com" \
-			eventsFrom:"${end_timestamp}" to:"${tonight}"
-	)
+	icalBuddy \
+		--includeEventProps "title,datetime" \
+		--propertyOrder "datetime,title" \
+		--noCalendarNames \
+		--dateFormat "%A" \
+		--timeFormat "%H:%M" \
+		--limitItems 1 \
+		--excludeAllDayEvents \
+		--separateByDate \
+		--bullet "" \
+		--excludeCals "training,omerxx@gmail.com" \
+		eventsFrom:"${end_timestamp}" to:"${tonight}")
 }
 
 parse_result() {
@@ -70,16 +70,16 @@ parse_result() {
 	for line in $1; do
 		array+=("$line")
 	done
-	time="${array[2]}"
+	time=$(echo "${array[2]}" | tr '\2' ' ')
 	end_time="${array[4]}"
 	title="${array[*]:5:30}"
 }
 
-calculate_times() {
-	epoc_meeting=$($DATE -j -f "%T" "$time:00" +%s)
-	epoc_now=$($DATE +%s)
+calculate_times(){
+	epoc_meeting=$(date -j -f "%T" "$time:00" +%s)
+	epoc_now=$(date +%s)
 	epoc_diff=$((epoc_meeting - epoc_now))
-	minutes_till_meeting=$((epoc_diff / 60))
+	minutes_till_meeting=$((epoc_diff/60))
 }
 
 display_popup() {
@@ -90,20 +90,20 @@ display_popup() {
 		-d '#{pane_current_path}' \
 		-T meeting \
 		icalBuddy \
-		--propertyOrder "datetime,title" \
-		--noCalendarNames \
-		--formatOutput \
-		--includeEventProps "title,datetime,notes,url,attendees" \
-		--includeOnlyEventsFromNowOn \
-		--limitItems 1 \
-		--excludeAllDayEvents \
-		--excludeCals "training" \
-		eventsToday
+			--propertyOrder "datetime,title" \
+			--noCalendarNames \
+			--formatOutput \
+			--includeEventProps "title,datetime,notes,url,attendees" \
+			--includeOnlyEventsFromNowOn \
+			--limitItems 1 \
+			--excludeAllDayEvents \
+			--excludeCals "training" \
+			eventsToday
 }
 
 print_tmux_status() {
-	if [[ $minutes_till_meeting -lt $ALERT_IF_IN_NEXT_MINUTES &&
-		$minutes_till_meeting -gt -60 ]]; then
+	if [[ $minutes_till_meeting -lt $ALERT_IF_IN_NEXT_MINUTES \
+		&& $minutes_till_meeting -gt -60 ]]; then
 		echo "$NERD_FONT_MEETING \
 			$time $title ($minutes_till_meeting minutes)"
 	else
@@ -127,6 +127,7 @@ main() {
 		calculate_times
 	fi
 	print_tmux_status
+	# echo "$minutes_till_meeting | $number_of_attendees"
 }
 
 main
