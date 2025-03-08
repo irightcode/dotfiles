4f923900d69d0a3227ea03e422d204f73255969f
irightcode <irightcode@gmail.com>
2025-03-07 19:21:30 -0500

    Function brews Added

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -400,3 +400,16 @@ duckduckgo() {
 wikipedia() {
     lynx -vikeys -accept_all_cookies "https://en.wikipedia.org/wiki?search=$@"
 }
+
+brews() {
+  local formulae="$(brew leaves | xargs brew deps --installed --for-each)"
+  local casks="$(brew list --cask 2>/dev/null)"
+
+  local blue="$(tput setaf 4)"
+  local bold="$(tput bold)"
+  local off="$(tput sgr0)"
+
+  echo "${blue}==>${off} ${bold}Formulae${off}"
+  echo "${formulae}" | sed "s/^\(.*\):\(.*\)$/\1${blue}\2${off}/"
+  echo "\n${blue}==>${off} ${bold}Casks${off}\n${casks}"
+}
